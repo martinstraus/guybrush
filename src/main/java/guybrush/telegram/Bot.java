ea8dbed87c1f1f804f4c21ab18658272077d9a08
Martin Straus <martin.straus@fit.com.ar>
2020-05-27 00:29:28 -0300

    Added daily greeting.

---
+++ src/main/java/guybrush/telegram/Bot.java
@@ -0,0 +1,30 @@
+package guybrush.telegram;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Martín Straus <martinstraus@gmail.com>
+ */
+@Component
+public class Bot {
+
+    private final Telegram telegram;
+    private final String username;
+
+    public Bot(
+            Telegram telegram, 
+            @Value("${guybrush.telegram.recipient_username}") String username
+    ) {
+        this.telegram = telegram;
+        this.username = username;
+    }
+
+    @Scheduled(cron = "0 9 * * * 1-5")
+    public void dailyEvent() {
+        telegram.send(username, "¡Buen día!");
+    }
+
+}
