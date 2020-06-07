2542477777e1d576fdc44c9ef8e2ae39a6f15b9d
Martin Straus <martin.straus@fit.com.ar>
2020-06-06 21:40:17 -0300

    Added grants on tables.

--- src/main/db/create-database.sql
+++ src/main/db/create-database.sql
@@ -1,3 +1,7 @@
 create database guybrush;
 create user guybrush with password 'guybrush';
 grant all privileges on database guybrush to guybrush;
+alter default privileges
+    for user guybrush
+    in schema public
+    grant select, insert, update, delete on tables to guybrush;
