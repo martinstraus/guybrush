dc1acd06792a09fb4ec69b34b351c7105c1b3352
Martín Gaspar Straus <martinstraus@gmail.com>
2022-11-09 20:56:33 -0300

    Don't send message if there're no reminders.

--- src/main/java/guybrush/telegram/Bot.java
+++ src/main/java/guybrush/telegram/Bot.java
@@ -3,6 +3,7 @@ package guybrush.telegram;
 import guybrush.reminders.Reminder;
 import guybrush.reminders.Reminders;
 import java.util.Set;
+import java.util.logging.Logger;
 import static java.util.stream.Collectors.joining;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Bot {
+
+    private static final Logger LOGGER = Logger.getLogger(Bot.class.getName());
 
     private final Reminders reminders;
     private final Telegram telegram;
@@ -31,13 +34,16 @@ public class Bot {
 
     @Scheduled(cron = "0 0 9 * * *")
     public void dailyEvent() {
-        telegram.send(username, messages(reminders.forToday()));
+        var remindersForToday = this.reminders.forToday();
+        if (!remindersForToday.isEmpty()) {
+            telegram.send(username, messages(remindersForToday));
+        } else {
+            LOGGER.info("No reminders for today");
+        }
     }
 
     private String messages(Set<Reminder> reminders) {
-        return reminders.isEmpty() 
-            ? "No hay recordatorios para hoy."
-            : reminders.stream().map(Reminder::message).collect(joining("\n"));
+        return reminders.stream().map(Reminder::message).collect(joining("\n"));
     }
 
     public void send(String message) {
