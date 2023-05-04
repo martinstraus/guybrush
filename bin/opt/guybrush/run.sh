0fee5864552fdb31d3bf56de08e48f580ebea254
Martin Straus <martinstraus@gmail.com>
2023-05-03 23:24:37 -0300

    Change maximum memory to 128M.

--- bin/opt/guybrush/run.sh
+++ bin/opt/guybrush/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/opt/jdk20/bin/java -Xms64M -Xmx64M -Dspring.config.additional-location=/etc/guybrush/application.properties -jar /opt/guybrush/guybrush.jar
+/opt/jdk20/bin/java \
+    -Xmx64M -XX:MaxRAM=64M \
+    -Dspring.config.additional-location=/etc/guybrush/application.properties \
+    -jar /opt/guybrush/guybrush.jar \
+    --server.port=8080
